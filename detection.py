fd754018362b03ea7270b2b1aca36427b66238ba
Ben <piela001>
2020-07-30 12:25:16 +0100

    Image slide test example

--- detection.py
+++ detection.py
@@ -26,7 +26,7 @@ import sys
 import threading
 import time
 
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image
 from picamera import PiCamera
 
 from aiy.board import Board
@@ -39,18 +39,16 @@ from aiy.vision.streaming import svg
 
 logger = logging.getLogger(__name__)
 
-JOY_COLOR = (255, 70, 0)
-SAD_COLOR = (0, 0, 64)
+JOY_COLOR = (0, 255, 0)
+SAD_COLOR = (0, 0, 255)
 
 JOY_SCORE_HIGH = 0.85
 JOY_SCORE_LOW = 0.10
 
 JOY_SOUND = ('C5q', 'E5q', 'C6q')
 SAD_SOUND = ('C6q', 'E5q', 'C5q')
-MODEL_LOAD_SOUND = ('C6w', 'c6w', 'C6w')
+MODEL_LOAD_SOUND = ('Cw', 'Eh', 'Gh', 'Fq')
 BEEP_SOUND = ('E6q', 'C6q')
-
-FONT_FILE = '/usr/share/fonts/truetype/freefont/FreeSans.ttf'
 
 BUZZER_GPIO = 22
 
@@ -65,11 +63,11 @@ def stopwatch(message):
         logger.info('%s done. (%fs)', message, end - begin)
 
 
-def run_inference(num_frames, on_loaded):
+def run_inference(on_loaded):
     """Yields (faces, (frame_width, frame_height)) tuples."""
     with CameraInference(face_detection.model()) as inference:
         on_loaded()
-        for result in inference.run(num_frames):
+        for result in inference.run():
             yield face_detection.get_faces(result), (result.width, result.height)
 
 
@@ -173,8 +171,7 @@ class Animator(Service):
         self.submit(joy_score)
 
 
-def joy_detector(num_frames, preview_alpha, image_format, image_folder,
-                 enable_streaming, streaming_bitrate, mdns_name):
+def joy_detector():
     done = threading.Event()
     def stop():
         logger.info('Stopping...')
@@ -182,97 +179,66 @@ def joy_detector(num_frames, preview_alpha, image_format, image_folder,
 
     signal.signal(signal.SIGINT, lambda signum, frame: stop())
     signal.signal(signal.SIGTERM, lambda signum, frame: stop())
-
+    
     logger.info('Starting...')
-    with contextlib.ExitStack() as stack:
-        leds = stack.enter_context(Leds())
-        board = stack.enter_context(Board())
-        player = stack.enter_context(Player(gpio=BUZZER_GPIO, bpm=10))
-        animator = stack.enter_context(Animator(leds))
-        # Forced sensor mode, 1640x1232, full FoV. See:
-        # https://picamera.readthedocs.io/en/release-1.13/fov.html#sensor-modes
-        # This is the resolution inference run on.
-        # Use half of that for video streaming (820x616).
-        camera = stack.enter_context(PiCamera(sensor_mode=4, resolution=(820, 616)))
-        stack.enter_context(PrivacyLed(leds))
+    leds = Leds()
+    board = Board()
+    player = Player(gpio=BUZZER_GPIO, bpm=10)
+    animator = Animator(leds)
 
-        server = None
-        if enable_streaming:
-            server = stack.enter_context(StreamingServer(camera, bitrate=streaming_bitrate,
-                                                         mdns_name=mdns_name))
-
-        def model_loaded():
-            logger.info('Model loaded.')
-            player.play(MODEL_LOAD_SOUND)
-
-        if preview_alpha > 0:
-            camera.start_preview(alpha=preview_alpha)
-
-        joy_moving_average = moving_average(10)
-        joy_moving_average.send(None)  # Initialize.
-        joy_threshold_detector = threshold_detector(JOY_SCORE_LOW, JOY_SCORE_HIGH)
-        joy_threshold_detector.send(None)  # Initialize.
-        for faces, frame_size in run_inference(num_frames, model_loaded):
-            joy_score = joy_moving_average.send(average_joy_score(faces))
-            animator.update_joy_score(joy_score)
-            event = joy_threshold_detector.send(joy_score)
-            if event == 'high':
-                logger.info('High joy detected.')
-                player.play(JOY_SOUND)
-            elif event == 'low':
-                logger.info('Low joy detected.')
-                player.play(SAD_SOUND)
-
-            if server:
-                server.send_overlay(svg_overlay(faces, frame_size, joy_score))
-
-            if done.is_set():
-                break
-
-def preview_alpha(string):
-    value = int(string)
-    if value < 0 or value > 255:
-        raise argparse.ArgumentTypeError('Must be in [0...255] range.')
-    return value
+    camera = PiCamera(sensor_mode=4, resolution=(820, 616))
+    PrivacyLed(leds)
 
 
-def main():
-    logging.basicConfig(level=logging.INFO)
+    def model_loaded():
+        logger.info('Model loaded.')
+        player.play(MODEL_LOAD_SOUND)
 
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--num_frames', '-n', type=int, default=None,
-                        help='Number of frames to run for')
-    parser.add_argument('--preview_alpha', '-pa', type=preview_alpha, default=0,
-                        help='Video preview overlay transparency (0-255)')
-    parser.add_argument('--image_format', default='jpeg',
-                        choices=('jpeg', 'bmp', 'png'),
-                        help='Format of captured images')
-    parser.add_argument('--image_folder', default='~/Pictures',
-                        help='Folder to save captured images')
-    parser.add_argument('--blink_on_error', default=False, action='store_true',
-                        help='Blink red if error occurred')
-    parser.add_argument('--enable_streaming', default=False, action='store_true',
-                        help='Enable streaming server')
-    parser.add_argument('--streaming_bitrate', type=int, default=1000000,
-                        help='Streaming server video bitrate (kbps)')
-    parser.add_argument('--mdns_name', default='',
-                        help='Streaming server mDNS name')
-    args = parser.parse_args()
+    joy_moving_average = moving_average(10)
+    joy_moving_average.send(None)  # Initialize.
+    joy_threshold_detector = threshold_detector(JOY_SCORE_LOW, JOY_SCORE_HIGH)
+    joy_threshold_detector.send(None)  # Initialize.
+    
+    directory = 'Photos'
+    for img_file in os.listdir(directory):
+        filename = os.fsdecode(img_file)
+        if filename.endswith(".jpg"):
+            img_path = os.path.join(directory, filename)
+            print("Opening " + img_path)
+            im =Image.open(img_path)
+            im.show()
+            emotion = detect_emotion(model_loaded, joy_moving_average, joy_threshold_detector, animator, player, done)
+            print(emotion)
+            im.close()
+    
+def detect_emotion(model_loaded, joy_moving_average, joy_threshold_detector, animator, player, done):
+    for faces, frame_size in run_inference(model_loaded):
+        joy_score = joy_moving_average.send(average_joy_score(faces))
+        animator.update_joy_score(joy_score)
+        event = joy_threshold_detector.send(joy_score)
+        if event == 'high':
+            logger.info('High joy detected.')
+            player.play(JOY_SOUND)
+            return "joy"
+        elif event == 'low':
+            logger.info('Low joy detected.')
+            player.play(SAD_SOUND)
+            return "sad"
+            
+        if done.is_set():
+            break
 
-    try:
-        joy_detector(args.num_frames, args.preview_alpha, args.image_format, args.image_folder,
-                     args.enable_streaming, args.streaming_bitrate, args.mdns_name)
-    except KeyboardInterrupt:
-        pass
-    except Exception:
-        logger.exception('Exception while running joy demo.')
-        if args.blink_on_error:
-            with Leds() as leds:
-                leds.pattern = Pattern.blink(100)  # 10 Hz
-                leds.update(Leds.rgb_pattern(Color.RED))
-                time.sleep(1.0)
+# def main():
+    # logging.basicConfig(level=logging.INFO)
 
-    return 0
+    # try:
+        # joy_detector()
+    # except KeyboardInterrupt:
+        # pass
+    # except Exception:
+        # logger.exception('Exception while running joy demo.')
 
-if __name__ == '__main__':
-    sys.exit(main())
+    # return 0
+
+# if __name__ == '__main__':
+    # sys.exit(main())
